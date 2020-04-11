2233bbdcc51700ed93953a863cce3beeb963dabb
Morten Stulen <stulenmorten@gmail.com>
2020-04-11 11:05:15 +0200

    Fix hamburger

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-import Navbar from "./components/navbar/Navbar";
-import { BrowserRouter } from "react-router-dom";
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import Navbar from './components/navbar/Navbar'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Routes from './navigation/Routes'
 ReactDOM.render(
   <React.StrictMode>
-    <Navbar />
-    <App />
+    <Router>
+      <Navbar />
+      <App />
+      <Routes />
+    </Router>
   </React.StrictMode>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
---
+++ src/navigation/Routes.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+import { Switch, Route } from 'react-router-dom'
+const Routes = () => {
+  return (
+    <Switch>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/projects">
+        <Projects />
+      </Route>
+      <Route path="/contact">
+        <Contact />
+      </Route>
+    </Switch>
+  )
+}
+export default Routes
+
+function Contact() {
+  return <h2>Contact</h2>
+}
+
+function About() {
+  return <h2>About me</h2>
+}
+
+function Projects() {
+  return <h2>Projects</h2>
+}
