20fee9b5a6d23774d675138393529dcef5833a20
Charlotte Marie Mitusch <cmmitusch@gmail.com>
2020-04-14 00:33:36 +0200

    start project

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import './Projects.css'
 
 const Projects = () => {
-  return <div>PROSJEKTER</div>
+  return (
+    <div className="about-page">
+      <section className="scroll">
+        <header className="project-header">
+          <h2 className="project-header-title">
+            <small id="project-header">EXPLORE MY</small> <br />
+            PROJECT PORTFOLIO
+          </h2>
+        </header>
+      </section>
+      <section className="scroll">
+        <div className="prosjekt-title">
+          <h2>EXPLORE</h2>
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export default Projects
