6a0571aad033b66ee2f89aa8097d32ea3bce31b8
Charlotte Marie Mitusch <cmmitusch@gmail.com>
2020-04-15 21:21:07 +0200

    props for classname

--- src/App.js
+++ src/App.js
@@ -6,62 +6,67 @@ import Box from './components/box/Box'
 import Bakgrunn from './components/background/Bakgrunn'
 import icon from '../src/assets/icons/bg.png'
 const App = () => {
-  useEffect(() => {
-    TxtType()
-  }, [])
-  return (
-    <div className="hjem">
-      <Bakgrunn
-        icon={icon}
-        header={'CHARLOTTE MITUSCH'}
-        text={'[ "BUILDING A PORTFOLIO", "PROJECT BY PROJECT"]'}
-      />
-      <div className="content">
-        <hr />
-        <div className="innhold">
-          <h2 className="title">
-            <small>WELCOME TO</small>
-          </h2>
-          <h2 className="undertittel"> MITUSCH CONSULTING</h2>
-          <hr id="margin" />
-          <div className="box-container1">
-            <Box
-              header={'MEET ME'}
-              body={
-                'Get to know the people who dedicated their careers to family law and who make it all happen for our clients.'
-              }
-              link={'/about'}
-              linkText={'ABOUT'}
-              image={bilde}
-            ></Box>
-          </div>
-          <hr />
-          <div>
-            <Box
-              className="left-centered-box"
-              header={'EXPLORE PROJECTS'}
-              body={
-                'Get to know the people who dedicated their careers to family law and who make it all happen for our clients.'
-              }
-              link={'/projects'}
-              linkText={'PROJECTS'}
-              image={bilde}
+    useEffect(() => {
+        TxtType()
+    }, [])
+    return (
+        <div className="hjem">
+            <Bakgrunn
+                icon={icon}
+                header={'CHARLOTTE MITUSCH'}
+                text={'[ "BUILDING A PORTFOLIO", "PROJECT BY PROJECT"]'}
             />
-          </div>
-          <hr />
-          <Box
-            header={'REACH OUT'}
-            body={
-              'Get to know the people who dedicated their careers to family law and who make it all happen for our clients.'
-            }
-            link={'/contact'}
-            linkText={'CONTACT'}
-            image={bilde}
-          />
+            <div className="content">
+                <hr />
+                <div className="innhold">
+                    <h2 className="title">
+                        <small>WELCOME TO</small>
+                    </h2>
+                    <h2 className="undertittel"> MITUSCH CONSULTING</h2>
+                    <hr id="margin" />
+                    <div className="box-container1">
+                        <Box
+                            className={'box-inner-container'}
+                            header={'MEET ME'}
+                            body={
+                                'Get to know the people who dedicated their careers to family law and who make it all happen for our clients.'
+                            }
+                            link={'/about'}
+                            linkText={'ABOUT'}
+                            imgName={'about3'}
+                            image={bilde}
+                        ></Box>
+                    </div>
+                    <hr />
+                    <div>
+                        <Box
+                            imgName={'about1'}
+                            className={'left-text-container'}
+                            header={'EXPLORE PROJECTS'}
+                            body={
+                                'Get to know the people who dedicated their careers to family law and who make it all happen for our clients.'
+                            }
+                            link={'/projects'}
+                            linkText={'PROJECTS'}
+                            image={bilde}
+                        />
+                    </div>
+                    <hr />
+                    <Box
+                        className={'box-inner-container'}
+                        imgName={'about3'}
+                        header={'REACH OUT'}
+                        body={
+                            'Get to know the people who dedicated their careers to family law and who make it all happen for our clients.'
+                        }
+                        link={'/contact'}
+                        linkText={'CONTACT'}
+                        image={bilde}
+                    />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default App
--- src/components/box/Box.js
+++ src/components/box/Box.js
@@ -1,23 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Box.css'
-const Box = ({ header, body, link, linkText, image }) => {
-  return (
-    <div className="box-container">
-      <div className="box-inner-container">
-        <div className="box">
-          <p className="top"> {header} </p> <p className="body"> {body} </p>{' '}
-          <Link to={link} className="link">
-            <p className="link-text"> {linkText} </p>{' '}
-            <i className="large material-icons link-icon">
-              keyboard_arrow_right{' '}
-            </i>{' '}
-          </Link>{' '}
-        </div>{' '}
-        <img src={image} alt="" className="about" />
-      </div>{' '}
-    </div>
-  )
+const Box = ({ className, header, body, link, linkText, image, imgName }) => {
+    if (className === 'box-inner-container' && imgName === 'about3') {
+    }
+    if (className === 'left-text-container' && imgName === 'about1') {
+    }
+    return (
+        <div className="box-container">
+            <div className="container">
+                <div className={className}>
+                    <div className="box">
+                        <p className="top"> {header} </p>{' '}
+                        <p className="body"> {body} </p>{' '}
+                        <Link to={link} className="link">
+                            <p className="link-text"> {linkText} </p>{' '}
+                            <i className="large material-icons link-icon">
+                                keyboard_arrow_right{' '}
+                            </i>{' '}
+                        </Link>{' '}
+                    </div>{' '}
+                    <img src={image} alt="" className={imgName} />
+                </div>{' '}
+            </div>
+        </div>
+    )
 }
 
 export default Box
